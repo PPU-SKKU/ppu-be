a8d924d9d108a575f9cc3ff4bfc26a8d1d3b35b4
yaongmeow <minnie_00@naver.com>
2025-01-24 20:03:35 +0900

    chore: 의존성 관련 파일 및 초기 파일

---
+++ src/main/java/com/ppu/ppu/PpuApplication.java
@@ -0,0 +1,13 @@
+package com.ppu.ppu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PpuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PpuApplication.class, args);
+	}
+
+}
