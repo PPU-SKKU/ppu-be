971cf4c1761ceca5cb2759d10861a47834bd8dea
yaongmeow <minnie_00@naver.com>
2025-08-02 16:41:39 +0900

    feat: PerfumeService 작성

--- src/main/java/com/ppu/ppu/perfume/PerfumeService.java
+++ src/main/java/com/ppu/ppu/perfume/PerfumeService.java
@@ -1,6 +1,9 @@
 package com.ppu.ppu.perfume;
 
+import com.ppu.ppu.perfume.dto.PerfumeResponseDto;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class PerfumeService {
@@ -8,5 +11,23 @@ public class PerfumeService {
 
     public PerfumeService(PerfumeRepository perfumeRepository) {
         this.perfumeRepository = perfumeRepository;
+    }
+
+    public List<PerfumeResponseDto> getAllPerfumes() {
+        return perfumeRepository.findAll().stream()
+                .map(PerfumeResponseDto::fromEntity)
+                .toList();
+    }
+
+    public PerfumeResponseDto getPerfumeById(Long perfumeId) {
+        return perfumeRepository.findById(perfumeId)
+                .map(PerfumeResponseDto::fromEntity)
+                .orElseThrow(() -> new RuntimeException("Perfume not found"));
+    }
+
+    public List<PerfumeResponseDto> searchPerfumesByKeyword(String searchKeyword) {
+        return perfumeRepository.findByKoreanNameOrOriginalName(searchKeyword).stream()
+                .map(PerfumeResponseDto::fromEntity)
+                .toList();
     }
 }
